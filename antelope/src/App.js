2b2dd1af186c5d755bc2d2fda0f431ce7ff4909c
Riley Hunter <rileyh@imagimationschool.com>
2022-05-19 23:35:17 +1200

    Yeehaw

--- antelope/src/App.js
+++ antelope/src/App.js
@@ -5,22 +5,40 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import config from './config';
 import React, { useState } from 'react'
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseApp = initializeApp(config);
 const db = getFirestore(firebaseApp);
-const someDocRef = doc(db, 'PooCollection', 'PeeDoc');
+
 
 function App() {
     const count = useState(0);
-    
+    let userName = useState('');
+    let userData = useState(null);
+
+    function handleUserNameChange(event) {
+        userName = event.target.value;
+        retrieveUserData(userName);
+    }
+
+    async function retrieveUserData(user) {
+        let docRef = doc(db, 'users', user);
+        let userSnap = await getDoc(docRef);
+        if (userSnap.exists()) {
+            userData = userSnap.data();
+            alert("Holy shit it worked");
+        }
+    }
+
     return (
         <div className="App">
+            <input type='text' placeholder='Enter your name' onChange={handleUserNameChange} />
             <Button text="Start a ranked climb" />
             <Button text="Start a casual climb" />
-            <Button text="Record a hangboard time" onClick={() => setDoc(someDocRef, { jorts: 'Bad' })}/>
+            <Button text="Record a hangboard time"/>
         </div>
     );
 }
+
 
 export default App;
