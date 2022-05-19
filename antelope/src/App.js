57073d55a3de5d60fa089645c3d798e74020f6fa
Riley Hunter <rileyh@imagimationschool.com>
2022-05-19 23:08:05 +1200

    Connection to Firebase

--- antelope/src/App.js
+++ antelope/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import config from './config';
+import React, { useState } from 'react'
+import { doc, setDoc } from "firebase/firestore";
+
+const firebaseApp = initializeApp(config);
+const db = getFirestore(firebaseApp);
+const someDocRef = doc(db, 'PooCollection', 'PeeDoc');
 
 function App() {
+    const count = useState(0);
+    
     return (
         <div className="App">
             <Button text="Start a ranked climb" />
             <Button text="Start a casual climb" />
-            <Button text="Record a hangboard time" />
+            <Button text="Record a hangboard time" onClick={() => setDoc(someDocRef, { jorts: 'Bad' })}/>
         </div>
     );
 }
---
+++ antelope/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAJt2KC0MsGVjO_NHQ5Tne1jnYxwE051Hk",
+    authDomain: "ant-elo-pe.firebaseapp.com",
+    projectId: "ant-elo-pe",
+    storageBucket: "ant-elo-pe.appspot.com",
+    messagingSenderId: "565124740259",
+    appId: "1:565124740259:web:ded67713795ae0b5283b16",
+    measurementId: "G-2BC78KMDS8"
+}
+
+export default config;
