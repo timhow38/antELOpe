cc3025eba5c87041d1e64b6bd7d55eb704fa4203
Riley Hunter <postagestamp@windowslive.com>
2022-05-24 16:04:36 +1200

    Added tentative ranks to climb attempt form

--- src/data/EloTools.js
+++ src/data/EloTools.js
@@ -2,7 +2,7 @@ function winProbability(thisElo, otherElo) {
 	return 1.0 / (1 + Math.pow(10, (otherElo - thisElo) / 400));
 }
 
-function getNextElo(thisElo, otherElo, outcome, developmentCoefficient) {
+function getNextElo(thisElo, otherElo, outcome, developmentCoefficient=30) {
 	return thisElo + (developmentCoefficient * (outcome - winProbability(thisElo, otherElo)));
 }
 
@@ -10,7 +10,7 @@ function reduceEloHistory(events, baseRating) {
 	let history = [];
 	events.filter(i => i.climbRating)
 		.reduce((prevElo, nextEvent) => {
-			let elo = getNextElo(prevElo, nextEvent.climbRating, nextEvent.outcome, 30);
+			let elo = getNextElo(prevElo, nextEvent.climbRating, nextEvent.outcome);
 			history.push({
 				startTime: nextEvent.startTime,
 				climbRating: nextEvent.climbRating,
@@ -28,4 +28,4 @@ function reduceElo(events, baseRating) {
 	return history[history.length - 1].currentElo;
 }
 
-export { reduceEloHistory, reduceElo };
+export { getNextElo, reduceEloHistory, reduceElo };
