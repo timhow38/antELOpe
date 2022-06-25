4eabd01fcc5e79f7fa709c491fbc46c02b6a8741
Timothy Howard <timhow38@gmail.com>
2022-06-25 12:32:25 +1200

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,22 @@ import { useContext } from 'react';
 import AppContext from './AppContext';
 import { Grid } from '@mui/material';
 
-
 function Header(props) {
     let [context, setContext] = useContext(AppContext);
 
     return <div id='nav-header'>
         {(
-             context.user && <Grid container spacing={2}>
-             <Grid item xs={4}>
-                 <Login />
-             </Grid>
-             <Grid item xs={4}>
-                 <CurrentElo />
-             </Grid>
-             <Grid item xs={4}>
-                 <RoutingButton text='Home' routeTarget='' />
-             </Grid>
-         </Grid>
+        context.user && <Grid container spacing={2}>
+            <Grid item xs={4}>
+                <Login />
+            </Grid>
+            <Grid item xs={4}>
+                <CurrentElo />
+            </Grid>
+            <Grid item xs={4}>
+                <RoutingButton text='Home' routeTarget='' />
+            </Grid>
+        </Grid>
         ) || <Login sx={{ width: '100%' }}/>}
     </div>
 }
